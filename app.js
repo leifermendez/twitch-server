48775c16701fc482c943f27900db8ea13e732d73
Leifer Mendez <leifer33@gmail.com>
2021-07-07 13:26:17 +0200

    Server

---
+++ app.js
@@ -0,0 +1,32 @@
+const NodeMediaServer = require('node-media-server');
+//C:\ffmpeg-4.4-full_build\bin
+//     ffmpeg: 'C:/ffmpeg-4.4-full_build/bin/ffmpeg.exe',
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http: {
+        port: 8000,
+        mediaroot: './media',
+        allow_origin: '*'
+    },
+    trans: {
+        ffmpeg: 'C:/ffmpeg-4.4-full_build/bin/ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+            }
+        ]
+    }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
